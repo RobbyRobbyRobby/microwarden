53cc482e6f9af11c48b52ee988830b719c5118c5
Rob <rlmartain@hotmail.com>
2022-10-19 13:02:27 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,44 @@
 input.onButtonPressed(Button.A, function () {
     music.setBuiltInSpeakerEnabled(true)
 })
+function ShowAware () {
+    HasHeardSomething = 1
+    led.setBrightness(128)
+    basic.showIcon(IconNames.SmallDiamond)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+}
+input.onButtonPressed(Button.AB, function () {
+    Initialize()
+})
 input.onButtonPressed(Button.B, function () {
     music.setBuiltInSpeakerEnabled(false)
 })
-music.setBuiltInSpeakerEnabled(true)
+function Initialize () {
+    music.setBuiltInSpeakerEnabled(false)
+    HasHeardSomething = 0
+    basic.clearScreen()
+}
+function ClearScreen () {
+    basic.clearScreen()
+}
+function ShowAngry () {
+    HasHeardSomething = 2
+    led.setBrightness(255)
+    basic.showIcon(IconNames.Angry)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+}
 let HasHeardSomething = 0
-basic.clearScreen()
+Initialize()
 basic.forever(function () {
     if (input.soundLevel() > 80 && HasHeardSomething < 2) {
-        HasHeardSomething = 2
-        basic.showIcon(IconNames.Angry)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+        ShowAngry()
     } else if (input.soundLevel() > 30 && HasHeardSomething < 1) {
-        HasHeardSomething = 1
-        basic.showIcon(IconNames.Asleep)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+        ShowAware()
     }
 })
 loops.everyInterval(3000, function () {
     if (input.soundLevel() < 10 && HasHeardSomething == 0) {
-        basic.clearScreen()
+        ClearScreen()
     }
     HasHeardSomething = 0
 })
