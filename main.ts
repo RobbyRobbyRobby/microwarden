fc192fb762b83301b47919e672c2036bc186e1ea
Rob <rlmartain@hotmail.com>
2022-10-20 14:28:43 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,44 @@
 radio.onReceivedNumber(function (receivedNumber) {
     ShowAngry(receivedNumber)
 })
+function FlashLEDs (numberOfFlashes: number) {
+    for (let index = 0; index < numberOfFlashes; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(200)
+    }
+    ClearScreen()
+}
 input.onButtonPressed(Button.A, function () {
     ShowAngry(9)
 })
 function ShowAware () {
 	
 }
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.B, function () {
     Initialize()
 })
-input.onButtonPressed(Button.B, function () {
-	
-})
 function Initialize () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
     radio.setGroup(1)
     music.setBuiltInSpeakerEnabled(false)
     basic.clearScreen()
 }
 function ClearScreen () {
     basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 }
 function ShowAngry (receivedNumber: number) {
     basic.showNumber(receivedNumber)
+    FlashLEDs(receivedNumber)
 }
 Initialize()
-basic.forever(function () {
-	
-})
