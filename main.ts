3470321e15d417e96e1f21d8bc4e6613560632ee
Rob <rlmartain@hotmail.com>
2022-10-20 14:18:11 +1100

    Removed a lot of code to be ONLY a warden, as there is now a seperate Shrieker/Sensor module

--- main.ts
+++ main.ts
@@ -1,44 +1,30 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    ShowAngry(receivedNumber)
+})
 input.onButtonPressed(Button.A, function () {
-    music.setBuiltInSpeakerEnabled(true)
+    ShowAngry(9)
 })
 function ShowAware () {
-    HasHeardSomething = 1
-    led.setBrightness(128)
-    basic.showIcon(IconNames.SmallDiamond)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+	
 }
 input.onButtonPressed(Button.AB, function () {
     Initialize()
 })
 input.onButtonPressed(Button.B, function () {
-    music.setBuiltInSpeakerEnabled(false)
+	
 })
 function Initialize () {
+    radio.setGroup(1)
     music.setBuiltInSpeakerEnabled(false)
-    HasHeardSomething = 0
     basic.clearScreen()
 }
 function ClearScreen () {
     basic.clearScreen()
 }
-function ShowAngry () {
-    HasHeardSomething = 2
-    led.setBrightness(255)
-    basic.showIcon(IconNames.Angry)
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+function ShowAngry (receivedNumber: number) {
+    basic.showNumber(receivedNumber)
 }
-let HasHeardSomething = 0
 Initialize()
 basic.forever(function () {
-    if (input.soundLevel() > 80 && HasHeardSomething < 2) {
-        ShowAngry()
-    } else if (input.soundLevel() > 30 && HasHeardSomething < 1) {
-        ShowAware()
-    }
-})
-loops.everyInterval(3000, function () {
-    if (input.soundLevel() < 10 && HasHeardSomething == 0) {
-        ClearScreen()
-    }
-    HasHeardSomething = 0
+	
 })
